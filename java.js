137a443773692374d94da6ed0d52af93149aba5f
ManojKBAccount <69326423+ManojKBAccount@users.noreply.github.com>
2020-08-07 02:55:36 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('#ms',{}); 
+sr.reveal('#msd',{delay: 200}); 
+sr.reveal('.h.h1',{delay: 200}); 
+sr.reveal('.bt',{delay: 200}); 
+sr.reveal('.image',{delay: 200});
+
+
+
+
+
+
+
